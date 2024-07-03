5b6e5081dde458c1de38c3a23ddacf268149c1bb
Juan Girini <juangirini@gmail.com>
2024-07-03 09:26:39 +0200

    cleanup private folders

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/juan/Projects/IdealLabs/etf-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/juan/Projects/IdealLabs/etf-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/juan/Projects/IdealLabs/etf-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/juan/Projects/IdealLabs/etf-docs/src/css/custom.css"),
-];
